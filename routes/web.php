6dd88155883e3f70073ecf106a224777e6528943
Mehmet Bulam <mehmet3bulam@gmail.com>
2021-10-27 18:08:00 +0000

    template eklendi

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Phone;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $phones= Phone::all();
+    return view('welcome',['phones'=>$phones]);
 });
 
 
